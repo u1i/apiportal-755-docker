be745fa648d4e4ce4cdd66828faa46e256ee0ac0
Uli Hitzel <uli.hitzel@gmail.com>
2019-03-01 00:27:17 +0800

    changed

--- installer-wrapper.sh
+++ installer-wrapper.sh
@@ -11,4 +11,10 @@ ln -s /tmp/printok /usr/bin/sestatus
 # Do install with expect
 echo 'portal ALL=(ALL) NOPASSWD: ALL' | sudo EDITOR='tee -a' visudo
 
+# Use expect to answer the questions
 /tmp/installer-wrapper.x
+
+# Change HTTP ports
+sudo sed -i 's/443/8443/g' /etc/httpd/conf.d/ssl.conf
+sudo sed -i 's/443/8443/g' /etc/httpd/conf.d/apiportal.conf
+sudo sed -i 's/80/8080/g' /etc/httpd/conf/httpd.conf
